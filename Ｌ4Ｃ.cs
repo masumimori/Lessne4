dd13fdbc05bdb84fc057e8b699a94de9082fa231
masumimori <31027890+masumimori@users.noreply.github.com>
2017-10-12 01:36:45 +0900

    Add files via upload

--- Ｌ4Ｃ.cs
+++ Ｌ4Ｃ.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class NewBehaviourScript : MonoBehaviour
+{
+    void Start()
+    {
+        // Use this for initialization
+        // 要素数5の配列を初期化する
+        int[] array = new int[5];
 
-    // Use this for initialization
-    // 要素数5の配列を初期化する
-    int[] array = new int[5];
+        // 配列の各要素に値を代入する
 
-    // 配列の各要素に値を代入する
-
-   　　 array[0] = 100;
+        array[0] = 100;
         array[1] = 200;
         array[2] = 500;
         array[3] = 700;
@@ -18,17 +20,17 @@ public class NewBehaviourScript : MonoBehaviour {
 
         // 5処理を繰り返す 関数の初期値は5から　5からインクリメントで１個つつ減る
 
-               for (int i = 5; i <= 5; i--) {
-                        
-                }
-        
+        for (int i = 4; i >= 5; i--) {
+
+        }
+
         // 配列表示
 
-                Debug.Log(array[i]);
-        }
+        Debug.Log(array[i]);
+    }
+}
 
     // Update is called once per frame
     void Update () {
 		
 	}
-}
